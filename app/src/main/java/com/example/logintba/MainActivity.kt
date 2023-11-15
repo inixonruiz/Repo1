6a5ced96fa594d92d8af7e42707cde882dd61097
Nixon <nixonruizc@gmail.com>
2023-11-15 15:19:15 -0600

    Version 1.0

--- app/src/main/java/com/example/logintba/MainActivity.kt
+++ app/src/main/java/com/example/logintba/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.logintba
 
 import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Parcel
 import android.os.Parcelable
+import android.text.method.LinkMovementMethod
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
@@ -25,6 +27,8 @@ class MainActivity() : AppCompatActivity() {
         val btnLogin : Button = findViewById(R.id.btnLogin)
         val edUsername : EditText = findViewById(R.id.edUsername)
         val edPassword : EditText = findViewById(R.id.edPassword)
+        val tvRegister : TextView = findViewById(R.id.tvRegister)
+
         btnLogin.setOnClickListener {
             val email = edUsername.text.toString().trim()
             val password = edPassword.text.toString().trim()
@@ -42,6 +46,12 @@ class MainActivity() : AppCompatActivity() {
             }else{
                Toast.makeText(this, "Input required", Toast.LENGTH_LONG).show()
             }
+        }
+
+
+        tvRegister.setOnClickListener{
+            val intent = Intent (this, RegisterActivity::class.java);
+            startActivity(intent)
         }
     }
 }
